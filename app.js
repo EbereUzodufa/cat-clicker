50e7a02847eea82f6c4ca5700b3f808145d46025
Ebere Uzodufa <ebereuzodufa@gmail.com>
2018-10-22 22:07:01 +0100

    Add files via upload
    feat: version 1.0.2
    
    Made the application two lovely cats. count the number of click for each then sums it up.

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-let clickCounter = 0;
+let clickCounter1 = 0;
+let clickCounter2 = 0;
 
 
-$('#theCat').click(function(e) {
+$('#theCat1').click(function(e) {
   //the element has been clicked... do stuff here
-  clickCounter++;
-  let ele = document.getElementById('clickCount');
-  ele.textContent = clickCounter;
+  clickCounter1++;
+  let ele = document.getElementById('clickCount1');
+  ele.textContent = clickCounter1;
+  updateTotal();
 });
+
+$('#theCat2').click(function(e) {
+  //the element has been clicked... do stuff here
+  clickCounter2++;
+  let ele = document.getElementById('clickCount2');
+  ele.textContent = clickCounter2;
+  updateTotal();
+});
+
+const updateTotal = function(){
+	let totalCount = clickCounter1 + clickCounter2;
+	console.log(totalCount);
+	$('#clickCountTotal').html(totalCount);
+	// $('#clickCountTotal').html(totalCount);
+}
